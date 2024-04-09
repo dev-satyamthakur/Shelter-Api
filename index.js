ed525ef5a390e7b306886064b47e00a9af50d355
Satyam Thakur <sat.thakur20@gmail.com>
2024-04-09 10:28:51 +0530

    Added update place endpoint

--- index.js
+++ index.js
@@ -150,6 +150,55 @@ app.post("/places", async (req, res) => {
   });
 });
 
+app.put("/places", async (req, res) => {
+  const { token } = req.cookies;
+  const {
+    id,
+    title,
+    address,
+    photos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+  } = req.body;
+
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,
+        address,
+        photos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+      });
+      await placeDoc.save();
+      res.json("ok");
+    }
+  });
+});
+
+app.get("/places", async (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await Place.findById(id));
+});
+
 const PORT = process.env.PORT || 4000; // Use PORT from environment variables or default to 4000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
